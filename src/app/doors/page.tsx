f74cc15b3c8250145fa55097c824f4273a507479
Joseph Williams <jw69455@gmail.com>
2025-04-07 15:29:13 -0500

    April Updates

--- src/app/doors/page.tsx
+++ src/app/doors/page.tsx
@@ -9,7 +9,7 @@ export default function DashboardPage() {
       <h1 className="text-3xl font-bold mb-6 text-blue-500">Electric Lounge Dashboard</h1>
       <Card className="overflow-hidden">
         <CardHeader className="py-4">
-          <CardTitle className="text-center text-pink-500 text-xl">Superlove RSVPs</CardTitle>
+          <CardTitle className="text-center text-pink-500 text-xl">April RSVPs</CardTitle>
         </CardHeader>
         <CardContent className="p-0 sm:p-2">
           <RSVPTable />
